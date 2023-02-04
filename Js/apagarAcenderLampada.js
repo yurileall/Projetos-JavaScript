b2c031967cea60fc4da07982ebf83004f1a171a8
yuri leal <yurileal111@outlook.com>
2023-02-04 01:38:41 -0300

    mudança do interruptor e ajuste de posição

--- Js/apagarAcenderLampada.js
+++ Js/apagarAcenderLampada.js
@@ -1,15 +1,49 @@
 
-$("#interruptor").click(() => {
+$("#interruptorOFF").click(() => {
     let x = document.getElementById("audioClick");
     x.play();
     $("#lampadaApagada").toggle()   
+    $("#interruptorOFF").toggle()   
 
     if($("#lampadaApagada").is(":visible")) {
         $("#lampadaAcessa").css("display", 'none')
+
        
     } else {
         $("#lampadaAcessa").css("display", 'flex')
        
     }
+    
+    if($("#linterruptorOFF").is(":visible")) {
+        $("#interruptorON").css("display", 'none')
+
+       
+    } else {
+        $("#interruptorON").css("display", 'flex')
+       
+    }
+})
+
+$("#interruptorON").click(() => {
+    let x = document.getElementById("audioClick");
+    x.play();
+    $("#lampadaApagada").toggle()
+    $("#interruptorON").toggle()   
+   
+
+    if($("#lampadaApagada").is(":visible")) {
+        $("#lampadaAcessa").css("display", 'none')
+
+       
+    } else {
+        $("#lampadaAcessa").css("display", 'flex')
+       
+    }
+
+    if($("#linterruptorON").not(":visible")) {
+        $("#interruptorOFF").css("display", 'flex')
+
+       
+    } 
 })
 
