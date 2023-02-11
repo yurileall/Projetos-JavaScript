f6f49a32cf299db1db3ec20058b902fdac736630
yuri leal <yurileal111@outlook.com>
2023-02-10 21:12:30 -0300

    implementando api

--- Js/previsaoDoTempo.js
+++ Js/previsaoDoTempo.js
@@ -1,6 +1,33 @@
-fetch('https://viacep.com.br/ws/01001000/json/').then(response => {
-    return response.json()
-}).then(corpo => {
-    //console.log(corpo)
-    document.getElementById("span").innerHTML=corpo.bairro
+const input = $("imput");
+const button = $("button");
+
+const place = $("#place")
+const temperatura = $("#temperatura")
+const img = $("img")
+const vento = $("#vento")
+const informacoes = $(".informacoes")
+
+$("button").click (() => {
+    if(!input.value) return;
+
+    getDataApi();
 })
+
+async function getDataApi(){
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${econdeURI(
+        input.value
+    )}&units=metrica&appid=0d13296f4e2976972fdb92ca20ec1e57`;
+
+    try{
+        await fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data?.cod && data.cod === "404") {
+                return alert ("Local não encontrado!");
+            }
+        });
+    } catch (error) {
+        alert(error);
+    }
+
+}
